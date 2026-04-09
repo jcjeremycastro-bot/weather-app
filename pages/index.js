b30b32473271a507787cb740a475e5cf50dfa3a6
Jeremy Castro <jc.jeremycastro@gmail.com>
2026-04-09 19:34:54 +0200

    refresh app every hour

--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,21 @@ import { ErrorScreen } from '../components/ErrorScreen'
 import styles from '../styles/Home.module.css'
 
 export const App = () => {
-  const [triggerFetch, setTriggerFetch] = useState(true)
   const [weatherData, setWeatherData] = useState()
 
   useEffect(() => {
-    const getData = async () => {
+    const fetchData = async () => {
       const res = await fetch('api/data')
       const data = await res.json()
       setWeatherData({ ...data })
     }
-    getData()
-  }, [triggerFetch])
+    fetchData()
+
+    // Refresh every hour
+    const interval = setInterval(fetchData, 3600000)
+
+    return () => clearInterval(interval)
+  }, [])
 
   return weatherData && !weatherData.message ? (
     <div className={styles.wrapper}>
@@ -33,13 +37,7 @@ export const App = () => {
       <MetricsBox weatherData={weatherData} />
     </div>
   ) : weatherData && weatherData.message ? (
-    <ErrorScreen errorMessage="City not found, try again!">
-      <Search
-        onFocus={e => (e.target.value = '')}
-        onChange={e => setCityInput(e.target.value)}
-        onKeyDown={e => e.keyCode === 13 && setTriggerFetch(!triggerFetch)}
-      />
-    </ErrorScreen>
+    <ErrorScreen errorMessage="City not found, try again!"></ErrorScreen>
   ) : (
     <LoadingScreen loadingMessage="Loading data..." />
   )
