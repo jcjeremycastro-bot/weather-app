53b356de3ebb0a790376db11267805db22d09afe
Jeremy Castro <jc.jeremycastro@gmail.com>
2026-04-09 12:47:54 +0200

    fetch city geocoding data with error handling and update normalized response structure

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,10 +1,19 @@
 export default async function handler(req, res) {
   const { cityInput } = req.body
 
+  const getCityGeocoding = await fetch(
+    `https://geocoding-api.open-meteo.com/v1/search?name=${cityInput}&count=1&language=fr&format=json&countryCode=FR`,
+  )
+  const geocodingData = await getCityGeocoding.json()
+
+  if (!geocodingData.results || geocodingData.results.length === 0) {
+    return res.status(404).json({ message: 'City not found' })
+  }
+
   const normalizedData = {
-    name: 'City Name',
+    name: geocodingData.results[0].name,
     sys: {
-      country: 'Country Code',
+      country: geocodingData.results[0].country_code,
       sunrise: 1640995200, // Unix timestamp
       sunset: 1641027600, // Unix timestamp
     },
